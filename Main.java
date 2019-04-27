2171a28f37338a1c9c607e714c31039d7ed2f52a
Elisee Mischael <elisee.kalonji99@gmail.com>
2019-04-27 12:44:59 +0200

    fixe menu

--- Main.java
+++ Main.java
@@ -5,16 +5,16 @@ public class Main{
     }
     public static void menu(){
         System.out.println("*****CONGO DATABASE*****");
-        System.out.println("=============================");
+        System.out.println("=============================================================");
         System.out.println("Le menu ci-dessous presente les differentes actions que vous pouvez executer.");
-        System.out.println("================================================================================");
-        System.out.println("Creer une BD: java Main -cbd");
-        System.out.println("Supprimer une BD: java Main -dbd");
-        System.out.println("Creer une table: java Main -ctb");
-        System.out.println("Supprimer une table: java Main -dtb");        
-        System.out.println("Inserer de donnees: java Main -idt");
-        System.out.println("Selectionner des donnees: java Main -sdata");
-        System.out.println("Mis a jours des donnees: java Main -udata");
-        System.out.println("Supprimer des donnees: java Main -dldata");
+        System.out.println("======================================================================================");
+        System.out.println("-Creer une BD: java Main -cbd");
+        System.out.println("-Supprimer une BD: java Main -dbd");
+        System.out.println("-Creer une table: java Main -ctb");
+        System.out.println("-Supprimer une table: java Main -dtb");        
+        System.out.println("-Inserer de donnees: java Main -idt");
+        System.out.println("-Selectionner des donnees: java Main -sdata");
+        System.out.println("-Mis a jours des donnees: java Main -udata");
+        System.out.println("-Supprimer des donnees: java Main -dldata");
     }
 }
