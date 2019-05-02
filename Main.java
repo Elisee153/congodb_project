0c98702e7ec153a8451eb8bc83711022d83ea1ea
Elisee Mischael <elisee.kalonji99@gmail.com>
2019-05-02 16:57:33 +0200

    styling the result in console

--- Main.java
+++ Main.java
@@ -1,20 +1,57 @@
+import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) {
+
         menu();
+        //recupere le choix        
+        Scanner scanner = new Scanner(System.in);
+        String choice = scanner.nextLine();
+        char choix = choice.charAt(0);
+        //============================
+        switch (choix) {                      
+            case '1':
+                break;
+            case '2':
+                break;
+            case '3':
+                break;
+            case '4':
+                break;
+            case '5':
+                break;
+            case '6':
+                break;
+            case '7':
+                break;
+            case '8':
+                break;
+            case '0':
+                quitter();
+                break;        
+            default:
+                break;
+        }
     }
     public static void menu(){
         System.out.println("*****CONGO DATABASE*****");
         System.out.println("=============================================================");
         System.out.println("Le menu ci-dessous presente les differentes actions que vous pouvez executer.");
-        System.out.println("======================================================================================");
-        System.out.println("-Creer une BD: java Main -cbd");
-        System.out.println("-Supprimer une BD: java Main -dbd");
-        System.out.println("-Creer une table: java Main -ctb");
-        System.out.println("-Supprimer une table: java Main -dtb");        
-        System.out.println("-Inserer de donnees: java Main -idt");
-        System.out.println("-Selectionner des donnees: java Main -sdata");
-        System.out.println("-Mis a jours des donnees: java Main -udata");
-        System.out.println("-Supprimer des donnees: java Main -dldata");
+        System.out.println("===============================================================================");
+        System.out.println("-Creer une BD: 1");
+        System.out.println("-Supprimer une BD: 2");
+        System.out.println("-Creer une table: 3");
+        System.out.println("-Supprimer une table: 4");        
+        System.out.println("-Inserer de donnees: 5");
+        System.out.println("-Selectionner des donnees: 6");
+        System.out.println("-Mis a jours des donnees: 7");
+        System.out.println("-Supprimer des donnees: 8");
+        System.out.println("Quitter: 0");
+        
+    }
+
+    public static void quitter(){
+        
+        System.out.println("Programme termine");
     }
 }
